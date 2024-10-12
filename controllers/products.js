5379b29cc82c826d46341ea8a5914626afe17dad
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-10-12 12:33:23 +0530

    Add regex

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,20 @@ const getAllProductsStatic = async (req, res) => {
   });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const allowedQueryParams = ["name", "price", "company", "rating"];
+  const query = {};
+
+  // Validate query parameters
+  for (const param of allowedQueryParams) {
+    if (req.query[param]) {
+      query[param] = req.query[param];
+    }
+  }
+  // Apply regex for 'name' only if it's present in the query
+  if (query.name) {
+    query.name = { $regex: query.name, $options: "i" }; // Case-insensitive regex for name
+  }
+  const products = await Product.find(query);
   res.status(200).json({
     nbHits: products.length,
     products,
