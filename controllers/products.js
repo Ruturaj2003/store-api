0f4632c0f7abd0d5f8b9da726cb42ca9b580be34
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-10-12 11:23:16 +0530

    Implement Dynamic Get all Products

--- controllers/products.js
+++ controllers/products.js
@@ -8,8 +8,10 @@ const getAllProductsStatic = async (req, res) => {
   });
 };
 const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
   res.status(200).json({
-    msg: "Fetch All Products...",
+    nbHits: products.length,
+    products,
   });
 };
 
